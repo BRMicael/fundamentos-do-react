0055301eb7a4b51d3fd239d2bbc21c46d058e6a4
Micael Batista <micaelbatista0108@hotmail.com>
2022-01-28 15:00:34 -0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,10 @@ import Contador from './components/contador/Contador'
 
 export default props => (
     <div className="App">
-    
+    <h1>Fundamentos React</h1> 
+
+    <div className="Cards">
+        
     <Card titulo="#01 - Primeiro componente" color="#ff80ff">
         <Primeiro/>
     </Card>
@@ -44,11 +47,11 @@ export default props => (
     <Card titulo="#06 - Condicional com If" color="#ff5660">
         <CondicionalComIf numero={11}/>
     </Card>
-    <Card titulo="#07 - Comunicação" color="#ff5660">
+    <Card titulo="#07 - Comunicação direta" color="#ff5660">
         <Pai sobrenome="Andrade"/>
     </Card>
 
-    <Card titulo="#08 - Comunicação" color="#ff5660">
+    <Card titulo="#08 - Comunicação indireta" color="#ff5660">
         <Super/>
     </Card>
 
@@ -56,9 +59,12 @@ export default props => (
         <Input/>
     </Card>
 
-    <Card titulo="#10 - Form" color="#ff5660">
+    <Card titulo="#10 - Contador" color="#ff5660">
         <Contador passo={10}/>
     </Card>
+
+    </div> 
+  
 
     </div>
 )
